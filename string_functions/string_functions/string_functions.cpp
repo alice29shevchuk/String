6a9f16676bcd197efbecbb784c950080907d11b3
Алиса Шевчук <alisa000077@gmail.com>
2021-12-21 11:05:26 +0200

    подсчитано количество букв и цифр

--- string_functions/string_functions/string_functions.cpp
+++ string_functions/string_functions/string_functions.cpp
@@ -1,6 +1,24 @@
 ﻿#include <iostream>
-
+#include<string>
+using namespace std;
 int main()
 {
-    
+    setlocale(0, "");
+    string str;
+    cout << "Введите строку: ";
+    getline(cin, str);
+    int end = 0;
+    int kolnumber = 0;
+    int kolword = 0;
+    while (str[end])
+    {
+        if (isalpha(str[end]))
+            kolword++;
+        if(isdigit(str[end]))
+            kolnumber++;
+        end++;
+    }
+    cout <<"Количество букв в строке = "<< kolword;
+    cout << "\n";
+    cout << "Количество цифр в строке = "<<kolnumber;
 }
