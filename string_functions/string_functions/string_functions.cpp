54f8d983a7c38e090d852c46c8179f2e7e9f141f
Алиса Шевчук <alisa000077@gmail.com>
2021-12-21 11:33:55 +0200

    добавлено поиск количества разделителей

--- string_functions/string_functions/string_functions.cpp
+++ string_functions/string_functions/string_functions.cpp
@@ -10,15 +10,20 @@ int main()
     int end = 0;
     int kolnumber = 0;
     int kolword = 0;
-    while (str[end])
+    int kolrazdel = 0;
+    while (str[end]) // пока не конец строки
     {
         if (isalpha(str[end]))
             kolword++;
         if(isdigit(str[end]))
             kolnumber++;
+        if (ispunct(str[end]) || isspace(str[end]))
+            kolrazdel++;
         end++;
     }
     cout <<"Количество букв в строке = "<< kolword;
     cout << "\n";
     cout << "Количество цифр в строке = "<<kolnumber;
+    cout << "\n";
+    cout << "Количество разделителей = " << kolrazdel;
 }
